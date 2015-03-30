24531c053295ff2850953f3378c11d813b08cc67
Julian <julfleischer@paypal.com>
2015-03-30 21:00:43 +0200

    whats up with uglify and node v0.11?

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,7 @@ module.exports = function (grunt) {
                     ]
                 },
                 options: {
-                    compress: true,
+                    compress: { drop_console: true },
                     report: "gzip",
                     preserveComments: "some",
                     banner: minifiedBanner,
